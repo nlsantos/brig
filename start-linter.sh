8f369e537c35b794025e3c3db20e6a8be2cc3cbe
Neil Santos <nlsantos@users.noreply.github.com>
2025-12-11 16:22:25 -0800

    dev: added Super-Linter config

---
+++ start-linter.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Convenience script to run locally the Super-Linter tool to perform
+# linting and formatting checks on the codebase as a whole.
+#
+# Requires a local installation of either Docker or podman.
+
+# Which command to use to run OCI commands; defaults to podman, but
+# will switch to Docker if podman is unavailable.
+OCI_CLI="podman"
+if ! which "${OCI_CLI}" 1>/dev/null 2>&1; then
+    OCI_CLI="docker"
+fi
+
+# Run Super-Linter locally with the current directory mounted as the
+# target
+"${OCI_CLI}" run --rm \
+    -e RUN_LOCAL=true \
+    -e DEFAULT_BRANCH="$(git rev-parse --abbrev-ref HEAD)" \
+    --env-file .super-linter.env \
+    -v "$(pwd)":/tmp/lint:Z \
+    ghcr.io/super-linter/super-linter:latest
