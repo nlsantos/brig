e2d6a5b26cce9f37331272da32afbd3f532da801
Neil Santos <nlsantos@users.noreply.github.com>
2025-12-11 16:35:35 -0800

    feat(writ): added the beginnings of a validating parser for devcontainer.json files

---
+++ writ/writ.go
@@ -0,0 +1,155 @@
+/*
+   writ: a devcontainer.json parser
+   Copyright (C) 2025  Neil Santos
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+*/
+
+// Package writ houses a validating parser for devcontainer.json files
+package writ
+
+import (
+	"bytes"
+	_ "embed"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log/slog"
+	"os"
+	"strings"
+
+	"github.com/nlsantos/brig/writ/internal/writ"
+	"github.com/santhosh-tekuri/jsonschema/v6"
+	"github.com/tailscale/hujson"
+)
+
+// devcontainerJsonSchema is the contents of the JSON schema against
+// which devcontainer.json files are validated.
+//
+//go:embed specs/devContainer.base.schema.json
+var devcontainerJsonSchema string
+
+// devcontainerJsonSchemaPath is the path used for the JSON schema
+// when being added manually as resource for the validator; it allows
+// the schema contents to be referenced by other resources later on.
+const devcontainerJsonSchemaPath = "devContainer.base.schema.json"
+
+// A Parser contains metadata about a target devcontainer.jkson file,
+// as well as the configuration for the intended devcontainer itself.
+type Parser struct {
+	Filepath      string                  // Path to the target devcontainer.json
+	Config        writ.DevcontainerConfig // The parsed contents of the target devcontainer.json
+	IsValidConfig bool                    // Whether or not the contents of the devcontainer.json conform to the spec; see p.Validate()
+
+	standardizedJSON []byte // The raw contents of the target devcontainer.json, converted to standard JSON
+}
+
+// NewParser returns a Parser targeting a devcontainer.json via
+// filepath. A few fields are initialized, and the returned Parser is
+// ready to perform additional operations.
+func NewParser(filepath string) Parser {
+	p := Parser{
+		Filepath:      filepath,
+		IsValidConfig: false,
+	}
+	stdJSON, err := p.standardizeJSON()
+	if err != nil {
+		panic(err)
+	}
+	p.standardizedJSON = stdJSON
+	return p
+}
+
+// Validate runs the contents of the target devcontainer.json against
+// a snapshot of the devcontainer spec's official JSON Schema.
+//
+// A successful validation operation returns err == nil and sets
+// p.IsValidConfig accordingly. Until after this is run, the value of
+// p.IsValidConfig should not be considered definitive.
+func (p *Parser) Validate() error {
+	slog.Debug("initializing JSON schema validator")
+	dcSchema, err := jsonschema.UnmarshalJSON(strings.NewReader(devcontainerJsonSchema))
+	if err != nil {
+		slog.Error("unable to unmarshal embedded JSON schema", "error", err)
+		return err
+	}
+	c := jsonschema.NewCompiler()
+	if err := c.AddResource(devcontainerJsonSchemaPath, dcSchema); err != nil {
+		slog.Error("unable to add embedded JSON schema as resource", "error", err)
+		return err
+	}
+	sch, err := c.Compile(devcontainerJsonSchemaPath)
+	if err != nil {
+		slog.Error(fmt.Sprintf("unable to compile JSON schema: %#v", err))
+		return err
+	}
+
+	slog.Debug("unmarshalling devcontainer.json", "path", p.Filepath)
+	valInput, err := jsonschema.UnmarshalJSON(bytes.NewReader(p.standardizedJSON))
+	if err != nil {
+		slog.Error("failed to unmarshal JSON for validation", "error", err)
+		return err
+	}
+
+	if err = sch.Validate(valInput); err != nil {
+		slog.Error("specified devcontainer.json failed schema validation", "path", p.Filepath)
+		return err
+	}
+
+	p.IsValidConfig = true
+	return nil
+}
+
+// Parse the contents of the target devcontainer.json into a struct.
+//
+// Will refuse to parse unless the contents are determined to conform
+// to the official JSON Schema spec.
+func (p *Parser) Parse() error {
+	if !p.IsValidConfig {
+		return errors.New("devcontainer.json flagged invalid")
+	}
+
+	slog.Debug("attempting to parse and unmarshal devcontainer.json")
+	if err := json.Unmarshal(p.standardizedJSON, &p.Config); err != nil {
+		slog.Error("failed to unmarshal JSON", "error", err, "path", p.Filepath)
+		return err
+	}
+
+	return nil
+}
+
+// Convert the contents of the target devcontainer.json, which could
+// be JSONC, into standard JSON suitable for validation and parsing.
+func (p *Parser) standardizeJSON() ([]byte, error) {
+	slog.Debug("attempting to standardize devcontainer.json contents", "path", p.Filepath)
+	file, err := os.Open(p.Filepath)
+	if err != nil {
+		slog.Error("failed to open devcontainer.json", "error", err, "path", p.Filepath)
+		return nil, err
+	}
+	defer file.Close()
+
+	fileInput, err := io.ReadAll(file)
+	if err != nil {
+		slog.Error("failed to read contents of devcontainer.json", "error", err, "path", p.Filepath)
+		return nil, err
+	}
+
+	stdJSON, err := hujson.Standardize(fileInput)
+	if err != nil {
+		slog.Error("failed to standardize devcontainer.json contents", "error", err, "path", p.Filepath)
+		return nil, err
+	}
+
+	return stdJSON, nil
+}
---
+++ writ/writ_test.go
@@ -0,0 +1,98 @@
+package writ
+
+import (
+	"io"
+	"log/slog"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// TestParse checks and illustrates the exepcted flow of parsing;
+// there are additional tests that exercise the parsing functionality
+func TestParse(t *testing.T) {
+	// Silence slog output for the duration of the run
+	slog.SetDefault(slog.New(slog.NewTextHandler(io.Discard, nil)))
+
+	p := NewParser(filepath.Join("testdata", "parse", "simple-devcontainer.json"))
+	// Parsing an unvalidated file should fail
+	if err := p.Parse(); err == nil {
+		t.Fatal("parsed an invalid/unvalidated devcontainer.json")
+	}
+	// A devcontasiner.json needs to be validated before being parsed
+	if err := p.Validate(); err != nil {
+		t.Fatal("devcontainer.json expected to be valid failed validation")
+	}
+	// This should now work
+	if err := p.Parse(); err != nil {
+		t.Fatal("devcontainer.json expected to be valid failed parsing")
+	}
+}
+
+// TestParseSimple parses a simple devcontainer.json and checks that
+// the unmarshalled values match as expected
+func TestParseSimple(t *testing.T) {
+	// Silence slog output for the duration of the run
+	slog.SetDefault(slog.New(slog.NewTextHandler(io.Discard, nil)))
+
+	p := NewParser(filepath.Join("testdata", "parse", "simple-devcontainer.json"))
+	if err := p.Validate(); err != nil {
+		t.Fatal("devcontainer.json expected to be valid failed validation")
+	}
+	if err := p.Parse(); err != nil {
+		t.Fatal("devcontainer.json expected to be valid failed parsing")
+	}
+
+	containerEnv := make(map[string]string)
+	// TODO: Implement interpolation for variables
+	containerEnv["APP_PATH"] = "${containerWorkspaceFolder}"
+	containerEnv["SHELL"] = "/bin/bash"
+
+	// Check fields against known values
+	assert.Equal(t, *p.Config.Name, "simple-ish devcontainer.json", "fields not matching")
+	assert.Equal(t, *p.Config.Context, "..", "fields not matching")
+	assert.Equal(t, *p.Config.DockerFile, "./Containerfile", "fields not matching")
+	assert.Equal(t, p.Config.ContainerEnv, containerEnv, "fields not matching")
+}
+
+// TestValidate attempts validation of known valid and invalid samples
+// of devcontainer.json files.
+func TestValidate(t *testing.T) {
+	// Silence slog output for the duration of the run
+	slog.SetDefault(slog.New(slog.NewTextHandler(io.Discard, nil)))
+
+	pathsValidSamples, err := filepath.Glob(filepath.Join("testdata", "validate", "valid-*.json"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(pathsValidSamples) < 1 {
+		t.Error("unable to find valid devcontainer.json samples")
+	} else {
+		t.Run("AgainstValidSamples", func(t *testing.T) {
+			for _, path := range pathsValidSamples {
+				p := NewParser(path)
+				if err := p.Validate(); err != nil {
+					t.Fatal(err)
+				}
+			}
+		})
+	}
+
+	pathsInvalidSamples, err := filepath.Glob(filepath.Join("testdata", "validate", "invalid-*.json"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(pathsInvalidSamples) < 1 {
+		t.Error("unable to find invalid devcontainer.json samples")
+	} else {
+		t.Run("AgainstInvalidSamples", func(t *testing.T) {
+			for _, path := range pathsInvalidSamples {
+				p := NewParser(path)
+				if err := p.Validate(); err == nil {
+					t.Fatal("known-invalid sample passed validation: ", path)
+				}
+			}
+		})
+	}
+}
