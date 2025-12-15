7bb85f91b7ad995463a28ea64c43959a01259784
Neil Santos <nlsantos@users.noreply.github.com>
2025-12-14 20:43:28 -0800

    feat(writ): added a custom unmarshaller to deal with mounts items

---
+++ writ/internal/writ/unmarshall.go
@@ -0,0 +1,42 @@
+package writ
+
+import (
+	"encoding/json"
+	"log/slog"
+	"strings"
+)
+
+// Custom unmarshaller for the MountElement type
+//
+// Because of this unmarshaller, a MountElement should never have its
+// String field be non-nil with a valid decontainer.json file
+func (m *MountElement) UnmarshalJSON(data []byte) error {
+	m.Mount = &Mount{}
+	if len(data) > 0 && data[0] == '{' {
+		return json.Unmarshal(data, m.Mount)
+	}
+
+	var mountString string
+	if err := json.Unmarshal(data, &mountString); err != nil {
+		return err
+	}
+
+	mountSegments := strings.Split(mountString, ",")
+	for _, segment := range mountSegments {
+		splitSegment := strings.SplitN(segment, "=", 2)
+		splitSegment[0] = strings.ToLower(strings.TrimSpace(splitSegment[0]))
+		splitSegment[1] = strings.TrimSpace(splitSegment[1])
+
+		switch splitSegment[0] {
+		case "source":
+			m.Mount.Source = &splitSegment[1]
+		case "target":
+			m.Mount.Target = splitSegment[1]
+		case "type":
+			m.Mount.Type = Type(strings.ToLower(splitSegment[1]))
+		default:
+			slog.Debug("ignoring unknown mount directive", "key", splitSegment[0], "value", splitSegment[1])
+		}
+	}
+	return nil
+}
