5f44aafe9261f4d980700b55c2e80dd042939ffa
Neil Santos <nlsantos@users.noreply.github.com>
2025-12-24 16:42:59 -0800

    build(release): bump version to 0.0.7-alpha

--- cmd/brig/main.go
+++ cmd/brig/main.go
@@ -19,7 +19,7 @@ package main
 import "github.com/nlsantos/brig/internal/brig"
 
 const AppName string = "brig"
-const AppVersion string = "0.0.6-alpha"
+const AppVersion string = "0.0.7-alpha"
 
 func main() {
 	brig.NewCommand(AppName, AppVersion)
