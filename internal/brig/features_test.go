a808812080853fea24b7f809a480d05add95c924
Neil Santos <nlsantos@users.noreply.github.com>
2026-02-01 13:34:23 +0800

    test(brig): added test case for features with dependsOn and devcontainer.json with overrideFeatureInstallOrder

--- internal/brig/features_test.go
+++ internal/brig/features_test.go
@@ -51,6 +51,55 @@ func TestParseDependsOnSimple(t *testing.T) {
 	}
 }
 
+func TestParseDependsOnWithOverride(t *testing.T) {
+	// Silence slog output for the duration of the run
+	slog.SetDefault(slog.New(slog.NewTextHandler(io.Discard, nil)))
+
+	// Config composition is done manually to bypass set up and
+	// constraints we don't really need nor want
+
+	dcParser, err := writ.NewDevcontainerParser(filepath.Join("testdata", "features-dependson", "devcontainer.json"))
+	assert.Nil(t, err)
+	assert.Nil(t, dcParser.Validate())
+	assert.Nil(t, dcParser.Parse())
+
+	cmd := Command{featureParsersLookup: make(map[string]*writ.DevcontainerFeatureParser)}
+
+	for _, feature := range []string{"alpha", "beta", "gamma", "delta"} {
+		p, err := writ.NewDevcontainerFeatureParser(filepath.Join("testdata", "features-dependson", fmt.Sprintf("%s.json", feature)), nil)
+		assert.Nil(t, err)
+		assert.Nil(t, p.Validate())
+		assert.Nil(t, p.Parse())
+
+		cmd.featureParsersLookup[fmt.Sprintf("./%s", feature)] = p
+	}
+
+	installOrder := &dcParser.Config.OverrideFeatureInstallOrder
+	installDAG, err := cmd.BuildFeaturesInstallationGraph(installOrder)
+	assert.Nil(t, err)
+
+	featureRoots := []string{}
+	roots := installDAG.GetRoots()
+	for len(roots) > 0 {
+		for featureID := range roots {
+			featureRoots = append(featureRoots, featureID)
+			installDAG.DeleteVertex(featureID)
+		}
+		roots = installDAG.GetRoots()
+	}
+
+	for installIdx, feature := range *installOrder {
+		if installIdx == len(*installOrder)-1 {
+			break
+		}
+
+		featureIdx := slices.Index(featureRoots, feature)
+		for _, laterFeature := range (*installOrder)[installIdx+1:] {
+			assert.Greater(t, slices.Index(featureRoots, laterFeature), featureIdx)
+		}
+	}
+}
+
 func TestParseOverrideFeatureInstallOrderStandalone(t *testing.T) {
 	// Silence slog output for the duration of the run
 	slog.SetDefault(slog.New(slog.NewTextHandler(io.Discard, nil)))
